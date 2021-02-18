a42c9fcd1d11206a8ca623f7cd8c404db6efda87
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2021-02-18 16:18:46 -0600

    Add files via upload

---
+++ CalculationMethods.py
@@ -0,0 +1,65 @@
+from sympy import *
+
+# Needed symbols
+x = symbols('x')
+
+
+def newton_raphson(function, x0, expected_error, imax):
+    """
+    Calculates the root estimate using the Newton-Raphson method
+    :param function: function to be evaluated
+    :param x0: initial point
+    :param expected_error: expected error in percent
+    :param imax: maximum number of iterations
+    :return: root, iteration numbers, and error
+    """
+    xr = x0
+    iteration = 0
+    current_error = float('inf')
+
+    while current_error >= expected_error and iteration < imax:
+        xr_old = xr
+        xr = xr_old - ((parse_expr(function).evalf(subs={x: xr_old})) /
+                       (diff(parse_expr(function)).evalf(subs={x: xr_old})))
+        iteration += 1
+
+        if xr != 0:
+            current_error = abs((xr - xr_old) / xr) * 100
+
+    return xr, iteration, current_error
+
+
+def fixed_point(function, x0, expected_error, imax):
+    """
+    Calculates the root estimate using the fixed point method
+    :param function: function to be evaluated
+    :param x0: initial point
+    :param expected_error: expected error in percent
+    :param imax: maximum number of iterations
+    :return: root, iteration numbers, and error
+    """
+    xr = x0
+    iteration = 0
+    current_error = float('inf')
+
+    while current_error >= expected_error and iteration < imax:
+        xr_old = xr
+        xr = parse_expr(function).evalf(subs={x: xr_old})
+        iteration += 1
+
+        if xr != 0:
+            current_error = abs((xr - xr_old) / xr) * 100
+    if current_error < expected_error:
+        return xr, iteration, current_error
+    else:
+        return None
+
+
+def calculate_circuit(voltage, resistance, power):
+
+    kirchhoff = f"{resistance} * x ** 2 - {voltage} * x + {power}"
+    result = fixed_point(kirchhoff, 0, 0.01, 100)
+    if result is not None:
+        return result[0]
+
+    return None
---
+++ GraphicInterface.py
@@ -0,0 +1,58 @@
+from tkinter import *
+import CalculationMethods as Cm
+
+
+def calculate():
+    try:
+        v = float(voltage.get())
+        r = float(resistance.get())
+        p = float(power.get())
+        current = Cm.calculate_circuit(v, r, p)
+        if current is not None:
+            current = round(float(current), 4)
+        else:
+            current = "El circuito no es posible"
+        result["text"] = current
+
+
+    except ValueError:
+        print("Entrada no correcta")
+
+
+
+
+
+def clear():
+    print("Clear")
+
+window = Tk()
+title = Label(window, text="Calculadora de corriente", bg="darkgreen", fg="white")
+title.config(font=("Arial", 20))
+title.pack(fill=X)
+
+voltage = Entry(window, font="Arial 20")
+voltage.pack()
+
+resistance = Entry(window, font="Arial 20")
+resistance.pack()
+
+power = Entry(window, font="Arial 20")
+power.pack()
+
+result = Label(window)
+result.pack()
+
+clear_button = Button(window, text="Reiniciar", padx=20, pady=10, command=clear)
+clear_button.pack(side="bottom")
+
+calculate_button = Button(window, text="Calcular", padx=20, pady=10, command=calculate)
+calculate_button.pack(side="bottom")
+
+
+
+
+window.geometry("700x700")
+window.config(bg="black")
+window.mainloop()
+
+
