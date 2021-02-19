3f3fc5ff1e8b8e20eb516b46ee9d305069b074a6
PingMaster99 <rui18259@uvg.edu.gt>
2021-02-18 21:53:36 -0600

    Updated clear and fixed point calculations

--- CalculationMethods.py
+++ CalculationMethods.py
@@ -4,31 +4,6 @@ from sympy import *
 x = symbols('x')
 
 
-def newton_raphson(function, x0, expected_error, imax):
-    """
-    Calculates the root estimate using the Newton-Raphson method
-    :param function: function to be evaluated
-    :param x0: initial point
-    :param expected_error: expected error in percent
-    :param imax: maximum number of iterations
-    :return: root, iteration numbers, and error
-    """
-    xr = x0
-    iteration = 0
-    current_error = float('inf')
-
-    while current_error >= expected_error and iteration < imax:
-        xr_old = xr
-        xr = xr_old - ((parse_expr(function).evalf(subs={x: xr_old})) /
-                       (diff(parse_expr(function)).evalf(subs={x: xr_old})))
-        iteration += 1
-
-        if xr != 0:
-            current_error = abs((xr - xr_old) / xr) * 100
-
-    return xr, iteration, current_error
-
-
 def fixed_point(function, x0, expected_error, imax):
     """
     Calculates the root estimate using the fixed point method
@@ -56,9 +31,15 @@ def fixed_point(function, x0, expected_error, imax):
 
 
 def calculate_circuit(voltage, resistance, power, error):
+    if power != 0:
+        kirchhoff = f"({resistance} * x ** 2 + {power}) / {voltage}"
+    else:
+        kirchhoff = f"{voltage} / {resistance}"
+    if resistance == 0 and power == 0:
+        return None
 
-    kirchhoff = f"{resistance} * x ** 2 - {voltage} * x + {power}"
-    result = fixed_point(kirchhoff, 0, error, 100)
+    starting_point = voltage / resistance if resistance > 0 else 0
+    result = fixed_point(kirchhoff, starting_point, error, 100)
     if result is not None:
         return result[0]
 
--- GraphicInterface.py
+++ GraphicInterface.py
@@ -2,7 +2,6 @@ from tkinter import *
 import CalculationMethods as Cm
 
 
-
 def calculate():
     try:
         v = float(voltage.get())
@@ -16,16 +15,16 @@ def calculate():
             current = "El circuito no es posible"
         result["text"] = current
 
-
     except ValueError:
-        print("Entrada no correcta")
-
-
-
+        result["text"] = "Entrada no correcta"
 
 
 def clear():
-    print("Clear")
+    voltage.delete(0, END)
+    resistance.delete(0, END)
+    power.delete(0, END)
+    error.delete(0, END)
+    result["text"] = ""
 
 
 window = Tk()
@@ -44,82 +43,56 @@ window.rowconfigure(5, minsize=30)
 window.rowconfigure(6, minsize=30)
 window.rowconfigure(7, minsize=30)
 
-
-
 title = Label(window, text="Calculadora de corriente", bg="#595358", fg="white")
 title.config(font=("Arial", 20))
-title.grid(column = 0, row = 0, columnspan = 5, sticky = "we")
-#title.pack(fill=X)
-
-
+title.grid(column=0, row=0, columnspan=5, sticky="we")
 
 voltage = Entry(window, font="Arial 20")
-voltage.grid(row = 1, column = 2 )
-#voltage.pack()
-vtitle = Label(window, text="Voltaje", bg="#3891A6", fg="BLACK")
-vtitle.config(font=("Arial", 20))
-vtitle.grid(row = 1, column = 1)
-
-
-
+voltage.grid(row=1, column=2)
+v_title = Label(window, text="Voltaje", bg="#3891A6", fg="BLACK")
+v_title.config(font=("Arial", 20))
+v_title.grid(row=1, column=1)
 
 resistance = Entry(window, font="Arial 20")
-resistance.grid(row = 2, column = 2 )
-#resistance.pack()
-rtitle = Label(window, text="Resistencia", bg="#3891A6", fg="BLACK")
-rtitle.config(font=("Arial", 20))
-rtitle.grid(row = 2, column = 1)
-
-
+resistance.grid(row=2, column=2)
+r_title = Label(window, text="Resistencia", bg="#3891A6", fg="BLACK")
+r_title.config(font=("Arial", 20))
+r_title.grid(row=2, column=1)
 
 power = Entry(window, font="Arial 20")
-power.grid(row = 3, column = 2 )
-#power.pack()
-ptitle = Label(window, text="Poder", bg="#3891A6", fg="BLACK")
-ptitle.config(font=("Arial", 20))
-ptitle.grid(row = 3, column = 1)
-
-
+power.grid(row=3, column=2)
+p_title = Label(window, text="Poder", bg="#3891A6", fg="BLACK")
+p_title.config(font=("Arial", 20))
+p_title.grid(row=3, column=1)
 
 error = Entry(window, font="Arial 20")
-error.grid(row = 4, column = 2 )
-#error.pack()
-etitle = Label(window, text="Error", bg="#3891A6", fg="BLACK")
-etitle.config(font=("Arial", 20))
-etitle.grid(row = 4, column = 1)
-
-
+error.grid(row=4, column=2)
+e_title = Label(window, text="Error", bg="#3891A6", fg="BLACK")
+e_title.config(font=("Arial", 20))
+e_title.grid(row=4, column=1)
 
 result = Label(window)
-result.grid(row = 6, column = 2 )
-#result.pack()
+result.grid(row=6, column=2)
 rstitle = Label(window, text="Resultado", bg="#3891A6", fg="BLACK")
 rstitle.config(font=("Arial", 20))
-rstitle.grid(row = 6, column = 1)
+rstitle.grid(row=6, column=1)
 
-#Change file location of image
-photo= PhotoImage (file = r"C:\Users\crade\Dropbox\Ingenieria Mecatronica\Cuarto Año     - 2021\Primer Semestre\Digital 2\Labs\Github\Metodos\Microproject1NM\circuit.png")
-imagen= Button(window, image=photo, padx = 50, pady =50)
-imagen.grid(row =8, column = 2)
+# Change file location of image
+photo = PhotoImage(
+    file=r"./circuit.png")
+imagen = Button(window, image=photo, padx=50, pady=50)
+imagen.grid(row=8, column=2)
 
 clear_button = Button(window, text="Reiniciar", padx=20, pady=10, command=clear, bg="#99c24d")
 clear_button.config(font=("Arial", 15))
-clear_button.grid(row = 11, column = 2 )
-#clear_button.pack(side="bottom")
-
-
+clear_button.grid(row=11, column=2)
 
 
 calculate_button = Button(window, text="Calcular", padx=20, pady=10, command=calculate, bg="#99c24d")
 calculate_button.config(font=("Arial", 15))
-calculate_button.grid(row = 12, column = 2 )
-#calculate_button.pack(side="bottom")
+calculate_button.grid(row=12, column=2)
 
 
 window.geometry("745x600")
 window.config(bg="#B2CEDE")
 window.mainloop()
-
-
-
-
