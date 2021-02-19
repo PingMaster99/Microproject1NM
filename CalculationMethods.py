effd4c3f04b205bf91f1e232571208a1d257850c
PingMaster99 <rui18259@uvg.edu.gt>
2021-02-19 00:28:26 -0600

    Docstring final files

--- CalculationMethods.py
+++ CalculationMethods.py
@@ -1,3 +1,15 @@
+"""
+    CalculationMethods.py
+
+    Calculates the current of a circuit
+    using Kirchhoff's voltage law
+
+    Dario Marroquin 18269 (dariomarroquin)
+    Pablo Ruiz 18259 (PingMaster99)
+
+    Version 1.0
+    Updated February 19, 2020
+"""
 from sympy import *
 
 # Needed symbols
@@ -24,7 +36,8 @@ def fixed_point(function, x0, expected_error, imax):
 
         if xr != 0:
             current_error = abs((xr - xr_old) / xr) * 100
-    if current_error < expected_error:
+    if current_error < expected_error and xr.is_real:
+        # DC current only uses real values
         return xr, iteration, current_error
     else:
         return None
@@ -40,28 +53,35 @@ def calculate_circuit(voltage, resistance, power, error, initial_point):
     :param initial_point: initial point of estimation
     :return: current flowing through the circuit
     """
+
+    # A voltage of 0 means the circuit has no current
     if voltage == 0:
         return 0
 
+    # If the bulb consumes power, we check for which solution is optimal
     if power != 0:
         kirchhoff = f"({resistance} * x ** 2 + {power}) / {voltage}"
-        kirchhoff1 = f"(({voltage} * x - {power})/{resistance}) ** 0.5"
-
         first_result = fixed_point(kirchhoff, initial_point, error, 100)
+
+        kirchhoff1 = f"(({voltage} * x - {power})/{resistance}) ** 0.5"
         second_result = fixed_point(kirchhoff1, initial_point, error, 100)
 
         if first_result is not None and second_result is not None:
             first_current = first_result[0]
             second_current = second_result[0]
 
+            # Lamp resistance based upon the assumption that the voltage source is close to its requirements
+            # Note that instead of a lamp it could be any electronic component that consumes power
             lamp_resistance = voltage ** 2 / power
 
+            # We calculate the lamp resistance based on the obtained currents
             first_lamp_resistance = (voltage - resistance * first_current) / first_current
             second_lamp_resistance = (voltage - resistance * second_current) / second_current
 
             first_current_distance = abs(lamp_resistance - first_lamp_resistance)
             second_current_distance = abs(lamp_resistance - second_lamp_resistance)
 
+            # The current which results in a resistance value for the lamp that is closer to reality is returned
             if first_current_distance < second_current_distance:
                 return first_current
             else:
@@ -76,6 +96,7 @@ def calculate_circuit(voltage, resistance, power, error, initial_point):
         else:
             return None
 
+    # When the power is 0, a simpler equation can be used
     elif resistance != 0:
         kirchhoff = f"{voltage} / {resistance}"
         result = fixed_point(kirchhoff, initial_point, error, 100)
--- GraphicInterface.py
+++ GraphicInterface.py
@@ -1,24 +1,49 @@
+# encoding: utf-8
+"""
+    GraphicInterface.py
+
+    Displays the circuit calculator graphically
+
+    Dario Marroquin 18269 (dariomarroquin)
+    Pablo Ruiz 18259 (PingMaster99)
+
+    Version 1.0
+    Updated February 19, 2020
+"""
 from tkinter import *
 import CalculationMethods as Cm
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Constants
+TITLE_SIZE = 15
+
 
 def calculate():
+    """
+    Calculates the current based on the user's inputs
+    """
     try:
+        # Parsing inputs
         v = float(voltage.get())
         r = float(resistance.get())
         p = float(power.get())
         e = float(error.get())
         i = float(initial_point.get())
+
+        # Current calculation
         current = Cm.calculate_circuit(v, r, p, e, i)
 
         if current is not None:
             current = round(float(current), 7)
             result["text"] = str(current) + " A"
+
         else:
+            # Calculation resulted in a null solution
             current = "Revise estimación y gráfica"
             result["text"] = current
+
+            # We plot the graph to make a new estimate or discard the circuit
             plt.clf()
             plt.close()
             fig, ax = plt.subplots(num="Resultado")
@@ -33,6 +58,12 @@ def calculate():
 
 
 def clear():
+    """
+    Clears the input fields.
+    Note that the initial estimation is preloaded with '1'
+    """
+    initial_point.delete(0, END)
+    initial_point.insert(0, "1")
     voltage.delete(0, END)
     resistance.delete(0, END)
     power.delete(0, END)
@@ -40,12 +71,15 @@ def clear():
     result["text"] = ""
 
 
+"""
+    GUI window with grid layout
+"""
 window = Tk()
 window.columnconfigure(0, minsize=100)
 window.columnconfigure(1, minsize=100)
 window.columnconfigure(2, minsize=100)
 window.columnconfigure(3, minsize=100)
-window.columnconfigure(4, minsize=100)
+window.columnconfigure(4, minsize=50)
 
 window.rowconfigure(0, minsize=30)
 window.rowconfigure(1, minsize=30)
@@ -56,63 +90,72 @@ window.rowconfigure(5, minsize=30)
 window.rowconfigure(6, minsize=30)
 window.rowconfigure(7, minsize=30)
 
+
+"""
+    Titles and input fields
+"""
 title = Label(window, text="Calculadora de corriente", bg="#595358", fg="white")
 title.config(font=("Arial", 20))
 title.grid(column=0, row=0, columnspan=5, sticky="we")
 
 voltage = Entry(window, font="Arial 20")
-voltage.grid(row=1, column=2)
-v_title = Label(window, text="Voltaje", bg="#3891A6", fg="BLACK")
-v_title.config(font=("Arial", 20))
-v_title.grid(row=1, column=1)
+voltage.grid(row=1, column=2, pady=(20, 0))
+v_title = Label(window, text="Voltaje (V)", bg="#3891A6", fg="BLACK")
+v_title.config(font=("Arial", TITLE_SIZE))
+v_title.grid(row=1, column=1, pady=(20, 0))
 
 resistance = Entry(window, font="Arial 20")
 resistance.grid(row=2, column=2)
-r_title = Label(window, text="Resistencia", bg="#3891A6", fg="BLACK")
-r_title.config(font=("Arial", 20))
-r_title.grid(row=2, column=1)
+r_title = Label(window, text="Resistencia (Ohms)", bg="#3891A6", fg="BLACK")
+r_title.config(font=("Arial", TITLE_SIZE))
+r_title.grid(row=2, column=1, padx=(0, 5))
 
 power = Entry(window, font="Arial 20")
 power.grid(row=3, column=2)
-p_title = Label(window, text="Poder", bg="#3891A6", fg="BLACK")
-p_title.config(font=("Arial", 20))
+p_title = Label(window, text="Potencia (W)", bg="#3891A6", fg="BLACK")
+p_title.config(font=("Arial", TITLE_SIZE))
 p_title.grid(row=3, column=1)
 
 error = Entry(window, font="Arial 20")
 error.grid(row=4, column=2)
-e_title = Label(window, text="Error", bg="#3891A6", fg="BLACK")
-e_title.config(font=("Arial", 20))
+e_title = Label(window, text="Error esperado (%)", bg="#3891A6", fg="BLACK")
+e_title.config(font=("Arial", TITLE_SIZE))
 e_title.grid(row=4, column=1)
 
 initial_point = Entry(window, font="Arial 20")
 initial_point.grid(row=5, column=2)
 i_title = Label(window, text="Punto inicial", bg="#3891A6", fg="BLACK")
-i_title.config(font=("Arial", 20))
+i_title.config(font=("Arial", TITLE_SIZE))
 i_title.grid(row=5, column=1)
 initial_point.insert(0, "1")
 
 result = Label(window)
 result.grid(row=6, column=2)
-rstitle = Label(window, text="Resultado", bg="#3891A6", fg="BLACK")
-rstitle.config(font=("Arial", 20))
-rstitle.grid(row=6, column=1)
+rs_title = Label(window, text="Resultado", bg="#3891A6", fg="BLACK")
+rs_title.config(font=("Arial", TITLE_SIZE))
+rs_title.grid(row=6, column=1)
 
-# Change file location of image
-photo = PhotoImage(
-    file=r"./circuit.png")
-imagen = Button(window, image=photo, padx=50, pady=50)
-imagen.grid(row=8, column=2)
+"""
+    Circuit picture
+"""
+photo = PhotoImage(file=r"./circuit.png")
+image = Button(window, image=photo, padx=0, pady=0)
+image.grid(row=8, column=1, pady=(0, 20))
+
+"""
+    Buttons
+"""
+calculate_button = Button(window, text="Calcular", padx=20, pady=10, command=calculate, bg="#99c24d")
+calculate_button.config(font=("Arial", 15))
+calculate_button.grid(row=8, column=2, pady=(20, 150))
 
 clear_button = Button(window, text="Reiniciar", padx=20, pady=10, command=clear, bg="#99c24d")
 clear_button.config(font=("Arial", 15))
-clear_button.grid(row=11, column=2)
+clear_button.grid(row=8, column=2, pady=(75, 0))
 
-
-calculate_button = Button(window, text="Calcular", padx=20, pady=10, command=calculate, bg="#99c24d")
-calculate_button.config(font=("Arial", 15))
-calculate_button.grid(row=12, column=2)
-
-
-window.geometry("745x600")
+"""
+    Window display
+"""
+window.geometry("700x520")
 window.config(bg="#B2CEDE")
 window.mainloop()
